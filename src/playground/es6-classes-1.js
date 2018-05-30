e66619a79cb86617b0b2943c689313b722003f90
Gustav Westberg <gustavwes@gmail.com>
2018-05-30 15:13:43 +0200

    Section 4 Lesson 25

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -13,11 +13,52 @@ class Person {
 }
 
 
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor(){
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+        if(this.hasMajor()){
+            description += ` Their major is ${this.major}`
+        }
+        return description;
+    }
+}
+
+class Traveller extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    hasHomeLocation(){
+        return !!this.homeLocation;
+    }
+    getGreeting() {
+        let getGreeting = super.getGreeting();
+        if(this.hasHomeLocation()){
+            getGreeting += ` I'm visiting from Göteborg`;
+        }
+        return getGreeting;
+    }
+}
+
+const traveller = new Traveller('Some hippe', 25, 'Wakanda');
+console.log(traveller.getGreeting());
+
+
 const me = new Person('Kratli Trufen', 28);
 console.log(me.getGreeting());
 console.log(me.getDescription());
 
-const other = new Person();
+const other = new Student();
 console.log(other.getGreeting());
 console.log(other.getDescription());
+
+const student1 = new Student('Gustav Westberg', 24, 'Programming')
+console.log(student1.getDescription());
 
